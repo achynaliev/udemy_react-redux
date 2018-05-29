6129163f73131850cf4af65104d3bb8fb1703fd0
Atai Chynaliev <chynaliev.ae@gmail.com>
2018-05-29 14:28:23 -0700

    api call

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyABUyZR4DDOLYZQ4Qh7FpQcxgUvwxM2W7c';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+})
 
 const App = () => {
   return (
